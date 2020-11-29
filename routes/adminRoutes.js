7b58c024756b23dff32dbc4362037387475fb6a9
RChuaChiaco <ronnster.pokemaster@gmail.com>
2020-11-30 01:06:02 +0800

    added edit about us page for admin, added routes for edit about us, edit contact us, edit faq

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -21,4 +21,36 @@ router.get('/summary-of-all-orders', (req, res) => {
   });
 });
 
+/*
+  Edit About Us Page
+*/
+router.get('/edit-aboutus', (req, res) => {
+  res. render('edit-aboutus', {
+    title: "Edit About Us",
+    layout: "admin"
+  });
+});
+
+/*
+  Edit FAQ Page
+*/
+router.get('/edit-faq', (req, res) => {
+  res. render('edit-faq', {
+    title: "Edit FAQ",
+    layout: "admin"
+  });
+});
+
+/*
+  Edit Contact Us Page
+*/
+router.get('/edit-contact', (req, res) => {
+  res. render('edit-contact', {
+    title: "Edit Contact Us",
+    layout: "admin"
+  });
+});
+
+
+
 module.exports = router;
