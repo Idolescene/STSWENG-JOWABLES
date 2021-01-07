c3ab5eb6985106143b875a91fbd7793767c64a9b
lexcyman <alexandra.mangune@gmail.com>
2021-01-07 17:15:46 +0800

    modified product model

--- models/product.js
+++ models/product.js
@@ -6,6 +6,7 @@ const productSchema = new mongoose.Schema({
   description: {type: String, required: true},
   category: {type: String, required: true},
   price: {type: Number, required: true},
+  status: {type: String, required: true},
   img: {type: String, required: true}
 });
 
