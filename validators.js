655db3cbee2c84e3511223273033b3b40ec50b4e
RChuaChiaco <ronnster.pokemaster@gmail.com>
2021-01-20 01:55:37 +0800

    fixed bugs regarding validation for login, shipping and profile pages; fixed whitespace-only inputs getting accepted; fixed error message not displaying in shipping page for invalid email; fixed invalid contact numbers getting accepted

--- validators.js
+++ validators.js
@@ -2,9 +2,9 @@ const { body } = require('express-validator');
 
 /*User Registration*/
 const userRegisterValidation = [
-  body('regun').not().isEmpty().withMessage("Username is required."),
-  body('regfn').not().isEmpty().withMessage("Full Name is required."),
-  body('regemail').not().isEmpty().withMessage("Email is required.").isEmail().withMessage("Please provide a valid email."),
+  body('regun').trim().not().isEmpty().withMessage("Username is required."),
+  body('regfn').trim().not().isEmpty().withMessage("Full Name is required."),
+  body('regemail').trim().not().isEmpty().withMessage("Email is required.").isEmail().withMessage("Please provide a valid email."),
   body('regpass').isLength({ min: 8 }).withMessage("Password needs to be at least 8 characters long."),
   body('regcpass').isLength({ min: 8 }).withMessage("Confirm password needs to be at least 8 characters long.")
     .custom((value, { req }) => {
@@ -23,23 +23,23 @@ const userLoginValidation = [
 
 /*User Update Shipping Details*/
 const updateShippingValidation = [
-  body('fullname').not().isEmpty().withMessage("Full Name is required."),
-  body('contno').isLength({ min: 11, max: 11 }).withMessage("Contact number should be 11 digits in length."),
-  body('houseno').not().isEmpty().withMessage("House Number is required."),
-  body('brngy').not().isEmpty().withMessage("Barangay is required."),
-  body('city').not().isEmpty().withMessage("City is required."),
-  body('prov').not().isEmpty().withMessage("Province is required."),
+  body('fullname').trim().not().isEmpty().withMessage("Full Name is required."),
+  body('contno').isLength({ min: 11, max: 11 }).withMessage("Contact number should be 11 digits in length.").isInt().withMessage("Contact number should only be composed of numbers."),
+  body('houseno').trim().not().isEmpty().withMessage("House Number is required."),
+  body('brngy').trim().not().isEmpty().withMessage("Barangay is required."),
+  body('city').trim().not().isEmpty().withMessage("City is required."),
+  body('prov').trim().not().isEmpty().withMessage("Province is required."),
 ];
 
 /*User Checkout Shipping*/
 const checkoutShippingValidation = [
-  body('fullname').not().isEmpty().withMessage("Full Name is required."),
-  body('contno').isLength({ min: 11, max: 11 }).withMessage("Contact number should be 11 digits in length."),
-  body('email').not().isEmpty().withMessage("Email is required.").isEmail().withMessage("Please provide a valid email."),
-  body('houseno').not().isEmpty().withMessage("House Number is required."),
-  body('brngy').not().isEmpty().withMessage("Barangay is required."),
-  body('city').not().isEmpty().withMessage("City is required."),
-  body('prov').not().isEmpty().withMessage("Province is required."),
+  body('fullname').trim().not().isEmpty().withMessage("Full Name is required."),
+  body('contno').isLength({ min: 11, max: 11 }).withMessage("Contact number should be 11 digits in length.").isInt().withMessage("Contact number should only be composed of numbers."),
+  body('email').trim().not().isEmpty().withMessage("Email is required.").isEmail().withMessage("Please provide a valid email."),
+  body('houseno').trim().not().isEmpty().withMessage("House Number is required."),
+  body('brngy').trim().not().isEmpty().withMessage("Barangay is required."),
+  body('city').trim().not().isEmpty().withMessage("City is required."),
+  body('prov').trim().not().isEmpty().withMessage("Province is required."),
   body('payment').not().isEmpty().withMessage("Please select a method of payment."),
 ];
 
