8b0d9eb0ed28cfd6c5f1caf83891a8a4b8b5a51d
lexcyman <alexandra.mangune@gmail.com>
2021-01-23 17:18:08 +0800

    update code for deployment

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,7 @@
 // connection.js initializes the MongoDB connection through Mongoose
 
 const mongoose = require('mongoose');
-const dbURL = "mongodb+srv://admin:1234@ccapdev-test-vqhod.mongodb.net/salawaldb?retryWrites=true&w=majority";
+const {dbURL} = require("../config");
 
 const options = {
   useNewUrlParser: true,
