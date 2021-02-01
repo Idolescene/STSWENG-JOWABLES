594a942df40485d9f8c4e444116a020c8a091fde
RChuaChiaco <ronnster.pokemaster@gmail.com>
2021-02-01 18:40:58 +0800

    removed access to edit faq, edit contact and edit aboutus pages

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -32,36 +32,6 @@ router.get('/summary-of-all-orders', (req, res) => {
   });
 });
 
-/*
-  Edit About Us Page
-*/
-router.get('/edit-aboutus', (req, res) => {
-  res. render('edit-aboutus', {
-    title: "Edit About Us",
-    layout: "admin"
-  });
-});
-
-/*
-  Edit FAQ Page
-*/
-router.get('/edit-faq', (req, res) => {
-  res. render('edit-faq', {
-    title: "Edit FAQ",
-    layout: "admin"
-  });
-});
-
-/*
-  Edit Contact Us Page
-*/
-router.get('/edit-contact', (req, res) => {
-  res. render('edit-contact', {
-    title: "Edit Contact Us",
-    layout: "admin"
-  });
-});
-
 // router.get('/edit-product-details/:slug', (req, res) => {
   router.get('/edit-product-details', (req, res) => {
   res.render('edit-product', {
