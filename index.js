1e21e17c5f5d45b463227c6e6b84d568aed24c70
Lexie <lexie@Lexies-MacBook-Air.local>
2021-02-01 09:12:53 +0800

    testing

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // imports
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const exphbs = require('express-handlebars');
