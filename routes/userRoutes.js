6fef36982c2b96cd95c73921eb1f6dcb7d6841b3
Lexie <lexie@Lexies-MacBook-Air.local>
2021-02-06 23:35:54 +0800

    fixed bug #2.016

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -114,6 +114,9 @@ router.get('/contact', (req, res) => {
               iglink: "www.instagram.com/SalawalCo",
               phonenum: "+ 63 961 801 4235",
               email: "salawalco.ph@gmail.com",
+              phone: "./img/phone-ringing.png",
+              media: "./img/social-media.png",
+              email: "./img/email.png",
               loggedIn: req.session.user,
               cartProducts: result.products
             });
@@ -125,6 +128,9 @@ router.get('/contact', (req, res) => {
               iglink: "www.instagram.com/SalawalCo",
               phonenum: "+ 63 961 801 4235",
               email: "salawalco.ph@gmail.com",
+              phone: "./img/phone-ringing.png",
+              media: "./img/social-media.png",
+              email: "./img/email.png",
               loggedIn: req.session.user,
               cartProducts: null
             });
